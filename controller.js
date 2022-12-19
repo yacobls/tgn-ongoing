1df6bd38a3f007c16e54de6a89e315fe8c3b0859
yacobls <yacob.lamrodo@gmail.com>
2022-12-19 23:33:52 +0700

    Display data from database su

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./connection');
 exports.index = function(req,res){
     response.ok("The REST API application is already running",res)
 };
+
+// Displays all data
+exports.showalldata = function(req,res){
+    connection.query('SELECT * FROM su', function(error, rows, fileds){
+        if(error){
+            connection.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
+};
--- routes.js
+++ routes.js
@@ -5,4 +5,7 @@ module.exports = function(app) {
 
     app.route('/')
         .get(jsonku.index);
+
+    app.route('/appear')
+        .get(jsonku.showalldata);
 }
